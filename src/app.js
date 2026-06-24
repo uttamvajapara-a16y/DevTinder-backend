88b339d1e76effd9f4d2995758991ba8a60a35fa
uttamvajapara-a16y <uttamvajapara@gmail.com>
2026-06-25 01:08:48 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://dev-tinder-web-nine-sand.vercel.app",
     process.env.VERCEL_FRONTEND_LINK
 ].filter(Boolean);
 
